710a9216aca455d38b3654ab7c7f2f060fce3e8e
Celia <celia.dehors@gmail.com>
2026-06-25 18:42:01 +0200

    Add template file and name selection

--- script.bat
+++ script.bat
@@ -1,2 +1,8 @@
-scp mycustomtemplate.png root@10.11.99.1:/usr/share/remarkable/templates/
-ssh -T root@10.11.99.1 < remarkableCommands.txt
+::Get parameters
+set /p templateFile="What's the template file? "
+set /p templateName="Enter template display name: "
+
+
+::Install template
+scp %templateFile% root@10.11.99.1:/usr/share/remarkable/templates/
+ssh -T root@10.11.99.1 "bash -s" < remarkableCommands.txt "\"%templateFile%"\" "%templateName%"
