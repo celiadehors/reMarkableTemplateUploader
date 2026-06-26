1fffc40138d89088a21fe319f59cd9f1734d0690
Celia <celia.dehors@gmail.com>
2026-06-26 08:16:11 +0200

    Clean code
    Refactor and optimize code

--- script.bat
+++ script.bat
@@ -1,3 +1,5 @@
+@echo off
+
 ::Get parameters
 set /p templateFile="What's the template file? "
 set /p templateName="Enter template display name: "
@@ -5,4 +7,4 @@ set /p templateName="Enter template display name: "
 
 ::Install template
 scp %templateFile% root@10.11.99.1:/usr/share/remarkable/templates/
-ssh -T root@10.11.99.1 "bash -s" < remarkableCommands.txt "\"%templateFile%"\" "%templateName%"
+ssh -T root@10.11.99.1 "bash -s" < remarkableCommands.txt """%templateFile%""" "%templateName%"
